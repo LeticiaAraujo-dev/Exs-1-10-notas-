3b4406b8ed0725a3528ada0adac268da751660e6
ĹeticiaAraujo-dev <leticia.p.araujo04@gmail.com>
2020-11-20 19:10:37 -0300

    começo do exer

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exs_1_10_notas_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+
+            for(var i = 0; i < 3; i++){
+                Console.WriteLine($"Digite o {i+1}º nome:");
+                nomes [i] = Console.ReadLine();
+                Console.WriteLine("Digite a 1º nota:");
+                notas [i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a 2º nota:");
+                notas [i+1] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite a 3º nota:");
+                notas [i+2] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
